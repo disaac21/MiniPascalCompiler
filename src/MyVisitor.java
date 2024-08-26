d6591e1932b175912d022ba745532f77e55a82b3
SerlioGiron <serlio.giron@unitec.edu>
2024-08-25 23:36:54 -0600

    borre el get text grande

--- src/MyVisitor.java
+++ src/MyVisitor.java
@@ -6,7 +6,7 @@ public class MyVisitor extends MiniPascalGrammarBaseVisitor<Object> {
     @Override
     public Object visitProgram(MiniPascalGrammarParser.ProgramContext ctx) {
 //        System.out.println("Archivo Vacío");
-        System.out.println(ctx.getText());
+//        System.out.println(ctx.getText());
         return visitChildren(ctx);
     }
 
