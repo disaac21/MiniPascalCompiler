553f8d534bd5faf2a41b970dd883126360ad6cb3
SerlioGiron <serlio.giron@unitec.edu>
2024-09-11 13:09:33 -0600

    hay que hacer pruebas

--- src/Manejo_Errores.java
+++ src/Manejo_Errores.java
@@ -21,37 +21,46 @@ public class Manejo_Errores extends BaseErrorListener {
 //        System.err.println("rule stack: "+stack);
 //        System.out.println(e);
         String errorType = (e == null) ? "Léxico" : "Sintáctico";
-//        String translatedMsg = translateErrorMessage(msg);
+        String translatedMsg = translateErrorMessage(msg, e, recognizer);
         System.err.println("Error de tipo: " + errorType);
-        System.err.println("linea "+line+" - " + "caracter " +charPositionInLine+" " + msg);
+        System.err.println("linea "+line+" - " + "caracter " +charPositionInLine+" " + translatedMsg);
         underlineError(recognizer,(Token)offendingSymbol,
                 line, charPositionInLine);
     }
-    private String translateErrorMessage(String msg) {
-        // Aquí puedes agregar más traducciones según sea necesario
-        if (msg.contains("mismatched input")) {
-            msg = msg.replace("mismatched input", "entrada no coincidente");
-            msg = msg.replace("expecting", "se esperaba");
+    private String translateErrorMessage(String msg, RecognitionException e, Recognizer<?, ?> recognizer) {
+        if (e instanceof NoViableAltException) {
+            msg = "No hay una alternativa viable en la entrada.";
+        } else if (e instanceof InputMismatchException) {
+            InputMismatchException ime = (InputMismatchException) e;
+            msg = "Entrada no coincidente. Cambielo por alguna de las siguientes opciones: " + ime.getExpectedTokens().toString(recognizer.getVocabulary());
+        } else if (e instanceof FailedPredicateException) {
+            msg = "Predicado fallido.";
+        } else if (e instanceof LexerNoViableAltException) {
+            msg = "No hay una alternativa viable en el lexer.";
+        } else {
+            msg = "Error de reconocimiento.";
         }
-
         return msg;
     }
     protected void underlineError(Recognizer recognizer,
                                   Token offendingToken, int line,
                                   int charPositionInLine) {
-        CommonTokenStream tokens =
-                (CommonTokenStream)recognizer.getInputStream();
-        String input = tokens.getTokenSource().getInputStream().toString();
-        String[] lines = input.split("\n");
-        String errorLine = lines[line - 1];
-        System.err.println(errorLine);
-        for (int i=0; i<charPositionInLine; i++) System.err.print(" ");
-        int start = offendingToken.getStartIndex();
-        int stop = offendingToken.getStopIndex();
-        if ( start>=0 && stop>=0 ) {
-            for (int i=start; i<=stop; i++) System.err.print("^");
+        if (recognizer.getInputStream() instanceof CommonTokenStream) {
+            CommonTokenStream tokens = (CommonTokenStream) recognizer.getInputStream();
+            String input = tokens.getTokenSource().getInputStream().toString();
+            String[] lines = input.split("\n");
+            String errorLine = lines[line - 1];
+            System.err.println(errorLine);
+            for (int i = 0; i < charPositionInLine; i++) System.err.print(" ");
+            int start = offendingToken.getStartIndex();
+            int stop = offendingToken.getStopIndex();
+            if (start >= 0 && stop >= 0) {
+                for (int i = start; i <= stop; i++) System.err.print("^");
+            }
+            System.err.println();
+        } else {
+//            System.err.println("Error: El input stream no es una instancia de CommonTokenStream.");
         }
-        System.err.println();
     }
 
     public int getErrorCount() {
